8e6e2821267aa75240d10affdf82583e3f383ac2
Wagner Silveira Souto <wagner.souto@ifood.com.br>
2024-03-24 15:50:33 -0300

    feature/Add income - outcome button styles

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,6 +1,6 @@
 import * as Dialog from '@radix-ui/react-dialog'
-import { CloseButton, Content, Overlay } from './styles'
-import { X } from 'phosphor-react'
+import { CloseButton, Content, Overlay, TransactionType, TransactionTypeButton } from './styles'
+import { ArrowCircleDown, ArrowCircleUp, X } from 'phosphor-react'
 
 export  function NewTransactionModal(){
     return(
@@ -15,6 +15,16 @@ export  function NewTransactionModal(){
                         <input type="text" required placeholder='Descrição'/>
                         <input type="number" required placeholder='Preço' />
                         <input type="text" required placeholder='Categoria' />
+                        <TransactionType>
+                            <TransactionTypeButton variant='income'>
+                                <ArrowCircleUp size={24}/>
+                                Entrada
+                            </TransactionTypeButton>
+                            <TransactionTypeButton variant='outcome'>
+                            <ArrowCircleDown size={24}/>
+                                Saída
+                            </TransactionTypeButton>
+                        </TransactionType>
                         <button type='submit'>
                             Cadastrar
                         </button>
